bb5faced50ced7d1aaf9f12189e244b7038cc4fb
cjdyer <cjdyer0@gmail.com>
2021-07-05 17:51:51 +1200

    Adding contact page

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFunction() { 
+    document.getElementById("title").style.display="none"; 
+    document.getElementById("arrow-container").style.display="block"; 
+}
+
+function goToInteralLink(path){
+    window.location.href = path;
+}
